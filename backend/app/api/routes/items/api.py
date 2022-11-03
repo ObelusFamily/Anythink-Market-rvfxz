72c073089e4932e26954705151221b1ed25213c5
Aykan Seper <96862833+bloodmoonman@users.noreply.github.com>
2022-11-03 20:16:49 +0000

    queries

--- backend/app/api/routes/items/api.py
+++ backend/app/api/routes/items/api.py
@@ -6,4 +6,3 @@ router = APIRouter()
 
 router.include_router(items_common.router, prefix="/items")
 router.include_router(items_resource.router, prefix="/items")
-router.include_router(items_resource.image.router, prefix="/items")
