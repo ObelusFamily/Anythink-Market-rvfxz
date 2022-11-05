7858b84a6f9b67ab4639a0748f6c54bd662916ac
Aykan Seper <96862833+bloodmoonman@users.noreply.github.com>
2022-11-05 11:00:55 +0000

    repo done

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -50,7 +50,7 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
                 description=description,
                 body=body,
                 seller_username=seller.username,
-                image=image or "../../placeholder.png"
+                image="../../placeholder.png"
             )
 
             if tags:
