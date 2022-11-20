bd326af74c8ad915cc54538348aa7546b3dc5643
Aykan Seper <96862833+bloodmoonman@users.noreply.github.com>
2022-11-20 14:53:47 +0000

    title query

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -136,32 +136,29 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
                 ).as_(
                     SELLER_USERNAME_ALIAS,
                 ),
+            ).where(items.title == title)
+        else:
+            query = Query.from_(
+                items,
+            ).select(
+                items.id,
+                items.slug,
+                items.title,
+                items.description,
+                items.body,
+                items.image,
+                items.created_at,
+                items.updated_at,
+                Query.from_(
+                    users,
                 ).where(
                     users.id == items.seller_id,
-                    items.title.like("%" + title + "%")
-                )
-        else:
-                query = Query.from_(
-                    items,
                 ).select(
-                    items.id,
-                    items.slug,
-                    items.title,
-                    items.description,
-                    items.body,
-                    items.image,
-                    items.created_at,
-                    items.updated_at,
-                    Query.from_(
-                        users,
-                    ).where(
-                        users.id == items.seller_id,
-                    ).select(
-                        users.username,
-                    ).as_(
-                        SELLER_USERNAME_ALIAS,
-                    ),)
-                
+                    users.username,
+                ).as_(
+                    SELLER_USERNAME_ALIAS,
+                ),
+            ) 
         # fmt: on
 
         if tag:
